b1cfea48d13b8aa34fc79b8b948c3fc402fc5378
iarts <ine.arts@uantwerpen.be>
2025-02-06 15:04:15 +0100

    log visualization edit

--- mandelbrot/visualisation.py
+++ mandelbrot/visualisation.py
@@ -1,15 +1,22 @@
 from matplotlib import pyplot as plt
-import numpy as np
-def show_image(mb_img):
+from matplotlib.colors import LogNorm
+import matplotlib as mpl
+
+cmap = mpl.colormaps.get_cmap('rainbow')
+cmap.set_bad(color='black')
+
+def show_image(mb_img,N):
     fig, ax = plt.subplots()
-    ax.imshow(mb_img, extent=[-2, 1, -4/3, 0])
-    ax.imshow(mb_img, extent=[-2, 1, 4/3, 0])
+    im = ax.imshow(mb_img, extent=[-2, 1, -4/3, 0], cmap=cmap, norm=LogNorm())
+    ax.imshow(mb_img, extent=[-2, 1, 4/3, 0], cmap=cmap, norm=LogNorm())
     ax.set_aspect(1)
     ax.set_ylim([-4/3,4/3])
+    fig.colorbar(im)
     plt.show()
 
 
 if __name__ == '__main__':
-    from mandelbrot_set import mandelbrot_img
-    img = mandelbrot_img()
-    show_image(img)
+    from mandelbrot_set import *
+    m = Mandelbrot(10000,5000)
+    img = mandelbrot_img(m)
+    show_image(img, m.N)
