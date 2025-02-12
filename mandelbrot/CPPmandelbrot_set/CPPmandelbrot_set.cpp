725c3faec7ef5136a6745dc64f80b4432d0259c5
iarts <ine.arts@uantwerpen.be>
2025-02-12 15:15:27 +0100

    load balancing of OMP threads

--- mandelbrot/CPPmandelbrot_set/CPPmandelbrot_set.cpp
+++ mandelbrot/CPPmandelbrot_set/CPPmandelbrot_set.cpp
@@ -19,7 +19,7 @@ mandelbrot( nb::ndarray<std::complex<double>, nb::ndim<2>>  c_val // in
 {
     size_t rows = c_val.shape(0);
     size_t cols = c_val.shape(1);
-    # pragma omp parallel for collapse(2)
+    # pragma omp parallel for collapse(2) schedule(runtime)
     for (size_t i = 0; i < rows; ++i) {
         for (size_t j = 0; j < cols; ++j) {
             std::complex<double> c = c_val(i, j);
